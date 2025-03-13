c30bfb9d6b680ab32f479f79f03c13227baaeebf
kobiowuquadri <kobiowuq@gmail.com>
2025-03-13 02:13:44 +0100

    feat: add mealing planning system

--- Back-End/routes/userRoutes/index.js
+++ Back-End/routes/userRoutes/index.js
@@ -1,14 +1,27 @@
 import express from 'express'
 import { loginController, registerController, verifyEmailController } from '../../controllers/Users/authControllers.js'
+import { 
+  createMealPlanningController,
+  getMealPlanningController,
+  getMealPlanningByIdController,
+  updateMealPlanningController,
+  deleteMealPlanningController
+} from '../../controllers/Users/mealPlanningController.js'
 import { upload } from '../../utils/index.js'
 import { verify } from '../../middlewares/auth-middleware.js'
+
 const userRouter = express.Router()
 
- 
+// Auth routes
 userRouter.post('/register', upload.single('profilePics'), registerController)
 userRouter.post('/login', loginController)
-userRouter.get('/verify-email', verifyEmailController) 
+userRouter.get('/verify-email', verifyEmailController)
 
-
+// Meal Planning routes (protected by auth middleware)
+userRouter.post('/meal-planning', verify, upload.single('recipeImage'), createMealPlanningController)
+userRouter.get('/meal-planning', verify, getMealPlanningController)
+userRouter.get('/meal-planning/:mealPlanId', verify, getMealPlanningByIdController)
+userRouter.put('/meal-planning/:mealPlanId', verify, upload.single('recipeImage'), updateMealPlanningController)
+userRouter.delete('/meal-planning/:mealPlanId', verify, deleteMealPlanningController)
 
 export default userRouter
---
+++ Back-End/services/Users/mealPlanningService.js
@@ -0,0 +1,93 @@
+import { MealPlanning } from '../../schema/Users/mealPlanningSchema.js'
+import { messageHandler } from '../../utils/index.js'
+import { BAD_REQUEST, SUCCESS, NOT_FOUND } from '../../constants/statusCode.js'
+import { cloudinary } from '../../utils/index.js'
+
+export const createMealPlanningService = async ({userId, data, file}, callback) => {
+  try {
+    if (!file) {
+      return callback(messageHandler("Recipe image is required", false, BAD_REQUEST, {}))
+    }
+
+    // Upload image to cloudinary
+    const result = await cloudinary.uploader.upload(file.path, {
+      folder: 'recipe_images'
+    })
+
+    const mealPlan = new MealPlanning({
+      userId,
+      ...data,
+      recipeImage: result.secure_url
+    })
+
+    return await mealPlan.save((err, mealPlan) => {
+      if (err) {
+        return callback(messageHandler("Failed to create meal plan", false, BAD_REQUEST, err))
+      }
+      return callback(messageHandler("Meal plan created successfully", true, SUCCESS, mealPlan))
+    })
+  } catch (error) {
+    return callback(messageHandler("Something went wrong...", false, BAD_REQUEST, error))
+  }
+}
+
+export const getMealPlanningService = async ({userId}, callback) => {
+  MealPlanning.find({ userId }).exec((err, mealPlans) => {
+    if (err) {
+      return callback(messageHandler("Failed to fetch meal plans", false, BAD_REQUEST, {}))
+    }
+    return callback(messageHandler("Meal plans fetched successfully", true, SUCCESS, mealPlans))
+  })
+}
+
+export const getMealPlanningByIdService = async ({userId, mealPlanId}, callback) => {
+  MealPlanning.findOne({ _id: mealPlanId, userId }).exec((err, mealPlan) => {
+    if (err) {
+      return callback(messageHandler("Failed to fetch meal plan", false, BAD_REQUEST, {}))
+    }
+    if (!mealPlan) {
+      return callback(messageHandler("Meal plan not found", false, NOT_FOUND, {}))
+    }
+    return callback(messageHandler("Meal plan fetched successfully", true, SUCCESS, mealPlan))
+  })
+}
+
+export const updateMealPlanningService = async ({userId, mealPlanId, data, file}, callback) => {
+  try {
+    const mealPlan = await MealPlanning.findOne({ _id: mealPlanId, userId })
+
+    if (!mealPlan) {
+      return callback(messageHandler("Meal plan not found", false, NOT_FOUND, {}))
+    }
+
+    if (file) {
+      const result = await cloudinary.uploader.upload(file.path, {
+        folder: 'recipe_images'
+      })
+      data.recipeImage = result.secure_url
+    }
+
+    Object.assign(mealPlan, data)
+
+    return mealPlan.save((err, updatedMealPlan) => {
+      if (err) {
+        return callback(messageHandler("Failed to update meal plan", false, BAD_REQUEST, err))
+      }
+      return callback(messageHandler("Meal plan updated successfully", true, SUCCESS, updatedMealPlan))
+    })
+  } catch (error) {
+    return callback(messageHandler("Something went wrong...", false, BAD_REQUEST, error))
+  }
+}
+
+export const deleteMealPlanningService = async ({userId, mealPlanId}, callback) => {
+  MealPlanning.findOneAndDelete({ _id: mealPlanId, userId }).exec((err, mealPlan) => {
+    if (err) {
+      return callback(messageHandler("Failed to delete meal plan", false, BAD_REQUEST, {}))
+    }
+    if (!mealPlan) {
+      return callback(messageHandler("Meal plan not found", false, NOT_FOUND, {}))
+    }
+    return callback(messageHandler("Meal plan deleted successfully", true, SUCCESS, {}))
+  })
+}
